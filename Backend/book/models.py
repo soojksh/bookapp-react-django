43f35365c4e10eea4408c91188e3eb3a65d1b884
sooj <soojksh@gmail.com>
2024-06-28 14:44:18 +0545

    book-model &book-serializer

--- Backend/book/models.py
+++ Backend/book/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+
+
